15e4ae8383672c1a3e19d1229ec82790f089844c
erikuhlin <eriknu@me.com>
2023-01-18 14:00:52 +0100

    test
    Change-Id: Ib9505cac092db2134b290fb6b7913e0b3b12d647

---
+++ Test Suites/Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>bc7fb380-801a-469c-b5f4-f9c4fc8bd79f</testSuiteGuid>
+   <testCaseLink>
+      <guid>22342810-e33d-49dc-b399-15821eee384c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sök bostad</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
