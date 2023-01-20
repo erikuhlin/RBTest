aa529de3fc2fc3c20ef8a799773cbaf65a434981
erikuhlin <eriknu@me.com>
2023-01-20 11:14:02 +0100

    suite update

--- Test Suites/Riksbyggen.se/Test suite 1.ts
+++ Test Suites/Riksbyggen.se/Test suite 1.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Suite</name>
+   <name>Test suite 1</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
